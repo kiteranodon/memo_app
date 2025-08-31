6851562bebb7a63bf41879a06aefd4eba7bc403c
kiteranodon <tidakakaka12@gmail.com>
2025-08-31 11:40:14 +0900

    一覧機能まで完成

--- resources/views/livewire/memos/show.blade.php
+++ resources/views/livewire/memos/show.blade.php
@@ -6,11 +6,10 @@ use App\Models\Memo;
 // ルートモデルバインディング
 state(['memo' => fn(Memo $memo) => $memo]);
 
-
-
 ?>
 
 <div>
+    <a href="{{ route('memos.index')}}">戻る</a>
     <h1>{{ $memo->title }}</h1>
     <p>{!! nl2br(e($memo->body)) !!}</p>
 </div>
